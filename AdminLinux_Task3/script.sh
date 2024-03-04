e4ef6b095bf79b239c8bebc7267805f68e5e7f2c
elnahassahmed34 <elnahassahmed34@gmail.com>
2024-03-04 20:04:13 +0200

    Initial commit for Task2_adminLinux.

---
+++ AdminLinux_Task3/script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if .bashrc file exists
+if [ -f ~/.bashrc ]; then
+    # Append environment variables to .bashrc
+    echo "export HELLO=$HOSTNAME" >> ~/.bashrc
+    echo "LOCAL=\$(whoami)" >> ~/.bashrc
+    
+    # Open another terminal
+    gnome-terminal &  
+
+    echo "Environment variables added to .bashrc. Another terminal opened."
+else
+    echo "Error: .bashrc file not found in the home directory."
+fi
+
